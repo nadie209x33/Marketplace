33574aae4ebeeeb057c46f265f1e453645464eb7
Sebaslong <sguerrero.sgc@gmail.com>
2025-08-30 18:37:38 -0300

    Se creo la clase List para dto

---
+++ back/src/main/java/com/uade/dto/List.java
@@ -0,0 +1,11 @@
+package com.uade.dto;
+
+import lombok.Data;
+
+@Data
+public class List {
+    private final int tlist_ID;
+    private final int list_ID;
+    private final int item_ID;
+    private final int quantity;
+}
