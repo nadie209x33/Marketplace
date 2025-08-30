48361d39246d0ab46407e8306ab3b8b5786da2af
Sebaslong <sguerrero.sgc@gmail.com>
2025-08-30 17:46:18 -0300

    Se creo la clase ListaDeEspera para dto

---
+++ back/src/main/java/com/uade/dto/ListaDeEspera.java
@@ -0,0 +1,10 @@
+package com.uade.dto;
+
+import lombok.Data;
+
+@Data
+public class ListaDeEspera {
+    private final int lde_ID;
+    private final int user_ID;
+    private final int item_ID;
+}
