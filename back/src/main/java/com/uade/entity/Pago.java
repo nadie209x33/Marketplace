76d6646b950344c829317d73c2e2f39dfa3755ce
Sebaslong <sguerrero.sgc@gmail.com>
2025-09-01 08:57:25 -0300

    Correcciones para Pago entity

--- back/src/main/java/com/uade/entity/Pago.java
+++ back/src/main/java/com/uade/entity/Pago.java
@@ -1,5 +1,7 @@
 package com.uade.entity;
 
+import java.time.Instant;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,32 +9,36 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@DaTa
+@Data
 @Entity
+@NoArgsConstructor
 @Table(name = "Pago")
 public class Pago {
 
-public Pago(){}
-public Pago(Int monto, String medio, Timestamp timestamp, String TX_ID){
-this.monto = monto;
-this.medio = medio
-This.timestamp = timestamp;
-this.TX_ID = TX_ID;
-}
-
 @Id
-@@GeneratedValue(strategy = GenerationType.IDENTITY)
-private Int pago_ID;
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int pago_ID;
 
 @Column(nullable = false)
-private Int monto;
+private int monto;
 
 @Column(nullable = false)
 private String medio;
 
 @Column(nullable = false)
-private Timestamp timestamp;
+private Instant timestamp;
+
+@Column(nullable = false)
+private int tx_ID;
+
+public Pago(int monto, String medio, Instant timestamp, int tx_ID){
+this.monto = monto;
+this.medio = medio;
+this.timestamp = timestamp;
+this.tx_ID = tx_ID;
+}
 
 
 }
