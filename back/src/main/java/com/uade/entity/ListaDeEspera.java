39908747625df54b9b34ae831248c3455096a6d0
Sebaslong <sguerrero.sgc@gmail.com>
2025-09-02 16:26:36 -0300

    #49 Correcciones para Lista de Espera

--- back/src/main/java/com/uade/entity/ListaDeEspera.java
+++ back/src/main/java/com/uade/entity/ListaDeEspera.java
@@ -5,7 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,16 +19,23 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "Lista_de_Espera")
+@Table(name = "Lista_de_Espera", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_ID", "item_ID"})
+    })
 public class ListaDeEspera {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "LDE_ID")
     private Integer lDEID;
 
-    //TODO agregar user_ID
-    //TODO agregar item_ID
-    //TODO agregar regla de uniquidad
+    private Integer ldeId;
 
+    @ManyToOne
+    @JoinColumn(name = "user_ID", referencedColumnName = "user_ID")
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "item_ID", referencedColumnName = "item_ID")
+    private Inventario item;
 }
 
