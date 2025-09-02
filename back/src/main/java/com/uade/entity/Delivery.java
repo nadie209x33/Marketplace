ec3575dab4d7d4064b8e49a0bf2bbe07b37f0ee6
Sebaslong <sguerrero.sgc@gmail.com>
2025-09-02 11:56:53 -0300

    #44 Se agrega el progreso de danna y se hacen cabios menores

---
+++ back/src/main/java/com/uade/entity/Delivery.java
@@ -0,0 +1,32 @@
+package com.uade.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Delivery" )
+public class Delivery {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "delivery_ID")
+    private Integer deliveryID;
+
+    @Column (nullable = false, name = "provider")
+    private String provider;
+    
+    //TODO agregar el address_ID
+    
+}
