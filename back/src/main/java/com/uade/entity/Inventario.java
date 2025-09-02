a1333fa868dfa93894380b1b70ef1a194652f08e
Sebaslong <sguerrero.sgc@gmail.com>
2025-09-02 16:24:59 -0300

    #47 Correccion de errores para Inventario

--- back/src/main/java/com/uade/entity/Inventario.java
+++ back/src/main/java/com/uade/entity/Inventario.java
@@ -1,10 +1,17 @@
 package com.uade.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,7 +28,7 @@ public class Inventario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "item_ID" )
-    private Integer itemID;
+    private Integer itemId;
 
     @Column(name = "description" ) 
     private String description;
@@ -35,10 +42,26 @@ public class Inventario {
     @Column(name = "price" )
     private Integer price;
 
-    //TODO Agregar cat_ID
-
     @Column (nullable = false, name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "cat_ID", referencedColumnName = "cat_ID")
+    private Categoria categoria;
+
+    @ManyToMany
+    @JoinTable(
+        name = "Image_List",
+        joinColumns = @JoinColumn(name = "item_ID"),
+        inverseJoinColumns = @JoinColumn(name = "img_ID")
+    )
+    private List<Image> images;
+
+    @OneToMany(mappedBy = "item")
+    private List<ListaDeEspera> listaDeEspera;
+
+    @OneToMany(mappedBy = "item")
+    private List<List> listItems;
 }
     
 
