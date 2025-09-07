1d65eaa0b2f86404ab1ec5e0346c042af2eaff74
Jeronimo <jeronimosuarezacosta@gmail.com>
2025-09-07 16:59:46 -0500

    Correción: Ahora usa solo DTOs

--- back/src/main/java/com/uade/back/controller/OrderController.java
+++ back/src/main/java/com/uade/back/controller/OrderController.java
@@ -4,33 +4,30 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-//import com.uade.back.service.order.OrderService;
-import com.uade.back.dto.order.CreateOrderRequest;
-import com.uade.back.dto.order.OrderResponse;
+import com.uade.back.dto.order.*;
+import com.uade.back.service.order.OrderService;
 
 import lombok.RequiredArgsConstructor;
 
-// @RestController
-// @RequestMapping("/api/v1/orders")
-// @RequiredArgsConstructor
-// public class OrderController {
+@RestController
+@RequestMapping("/api/v1/orders")
+@RequiredArgsConstructor
+public class OrderController {
 
-//   private final OrderService service;
+  private final OrderService service;
 
-//   @PostMapping
-//   public ResponseEntity<OrderResponse> create(@RequestBody CreateOrderRequest request) {
-//     return ResponseEntity.ok(service.create(request));
-//   }
+  @PostMapping
+  public ResponseEntity<OrderResponse> create(@RequestBody CreateOrderRequest request) {
+    return ResponseEntity.ok(service.create(request));
+  }
 
-//   @GetMapping("/{id}")
-//   public ResponseEntity<OrderResponse> get(@PathVariable Long id) {
-//     return ResponseEntity.ok(service.getById(id));
-//   }
+  @PostMapping("/by-id")
+  public ResponseEntity<OrderResponse> get(@RequestBody OrderIdRequest request) {
+    return ResponseEntity.ok(service.getById(request));
+  }
 
-//   @GetMapping
-//   public ResponseEntity<List<OrderResponse>> myOrders(
-//       @RequestParam(defaultValue = "0") int page,
-//       @RequestParam(defaultValue = "20") int size) {
-//     return ResponseEntity.ok(service.getMyOrders(page, size));
-//   }
-// }
+  @PostMapping("/search")
+  public ResponseEntity<List<OrderResponse>> myOrders(@RequestBody OrderListRequest request) {
+    return ResponseEntity.ok(service.getMyOrders(request));
+  }
+}
