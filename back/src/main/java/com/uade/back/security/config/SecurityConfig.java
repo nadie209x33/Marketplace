60391727c865e22a24266541842a21b02dce9ea0
Sebaslong <sguerrero.sgc@gmail.com>
2025-10-17 21:51:16 -0300

    Update SecurityConfig.java

--- back/src/main/java/com/uade/back/security/config/SecurityConfig.java
+++ back/src/main/java/com/uade/back/security/config/SecurityConfig.java
@@ -1,25 +1,23 @@
 package com.uade.back.security.config;
 
-import java.util.Arrays;
+import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
+import com.uade.back.entity.Role;
+import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
-import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import com.uade.back.entity.Role;
-
-import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
@@ -30,40 +28,109 @@ public class SecurityConfig {
     private final AuthenticationProvider authenticationProvider;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+        throws Exception {
         http
-                .cors(withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(req -> req.requestMatchers("/api/v1/auth/register").permitAll()
-                        .requestMatchers("/api/v1/auth/authenticate").permitAll()
-                        .requestMatchers("/api/v1/auth/activate").permitAll()
-                        .requestMatchers("/api/v1/auth/me").authenticated()
-                        .requestMatchers("/error/**").permitAll()
-                        .requestMatchers("/health").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/v1/products/**").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/v1/products/**").hasAuthority(Role.ADMIN.name())
-                        .requestMatchers(HttpMethod.PUT, "/api/v1/products/**").hasAuthority(Role.ADMIN.name())
-                        .requestMatchers(HttpMethod.DELETE, "/api/v1/products/**").hasAuthority(Role.ADMIN.name())
-                        .requestMatchers(HttpMethod.POST, "/api/v1/images/**").hasAuthority(Role.ADMIN.name())
-                        .requestMatchers(HttpMethod.DELETE, "/api/v1/images/**").hasAuthority(Role.ADMIN.name())
-                        .requestMatchers("/api/v1/users/**").hasAuthority(Role.ADMIN.name())
-                        .requestMatchers("/api/v1/addresses/**").hasAnyAuthority(Role.USER.name(), Role.ADMIN.name())
-                        .requestMatchers(HttpMethod.PATCH, "/api/v1/orders/payment/**").hasAuthority(Role.ADMIN.name())
-                        .requestMatchers(HttpMethod.PATCH, "/api/v1/orders/{orderId}/delivery-status").hasAuthority(Role.ADMIN.name())
-                        .requestMatchers(HttpMethod.POST, "/api/v1/orders/{orderId}/retry-payment").hasAnyAuthority(Role.USER.name(), Role.ADMIN.name())
-                        .requestMatchers("/categories/**").hasAnyAuthority(Role.USER.name())
-                        .requestMatchers(
-                                        "/swagger-ui.html",
-                                        "/swagger-ui/**",
-                                        "/v3/api-docs/**",
-                                        "/swagger-resources/**",
-                                        "/webjars/**"
-                                        ).permitAll()
-                        .anyRequest()
-                        .authenticated())
-                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+            .cors(withDefaults())
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(req ->
+                req
+                    // Public endpoints
+                    .requestMatchers(
+                        "/api/v1/auth/register",
+                        "/api/v1/auth/authenticate",
+                        "/api/v1/auth/activate"
+                    )
+                    .permitAll()
+                    .requestMatchers(
+                        HttpMethod.GET,
+                        "/api/v1/products",
+                        "/api/v1/categories/tree",
+                        "/api/v1/images/{imageId}"
+                    )
+                    .permitAll()
+                    .requestMatchers("/health", "/error/**")
+                    .permitAll()
+                    .requestMatchers(
+                        "/swagger-ui.html",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**",
+                        "/swagger-resources/**",
+                        "/webjars/**"
+                    )
+                    .permitAll()
+                    // User endpoints
+                    .requestMatchers(
+                        "/api/v1/auth/me",
+                        "/api/v1/auth/change-password"
+                    )
+                    .hasAuthority(Role.USER.name())
+                    .requestMatchers(
+                        HttpMethod.GET,
+                        "/api/v1/products/{productId}",
+                        "/api/v1/categories/byid/{categoryId}"
+                    )
+                    .hasAuthority(Role.USER.name())
+                    .requestMatchers("/api/v1/cart/**")
+                    .hasAuthority(Role.USER.name())
+                    .requestMatchers("/api/v1/addresses/**")
+                    .hasAuthority(Role.USER.name())
+                    .requestMatchers(HttpMethod.POST, "/api/v1/orders")
+                    .hasAuthority(Role.USER.name())
+                    .requestMatchers(HttpMethod.GET, "/api/v1/orders/my-orders")
+                    .hasAuthority(Role.USER.name())
+                    .requestMatchers(
+                        HttpMethod.POST,
+                        "/api/v1/orders/{orderId}/retry-payment"
+                    )
+                    .hasAuthority(Role.USER.name())
+                    .requestMatchers(HttpMethod.PUT, "/api/v1/users/me")
+                    .hasAuthority(Role.USER.name())
+                    // Admin endpoints
+                    .requestMatchers("/api/v1/admin/**")
+                    .hasAuthority(Role.ADMIN.name())
+                    .requestMatchers(
+                        "/api/v1/users/{userId}/upgrade",
+                        "/api/v1/users/{userId}/downgrade"
+                    )
+                    .hasAuthority(Role.ADMIN.name())
+                    .requestMatchers(HttpMethod.POST, "/api/v1/products/**")
+                    .hasAuthority(Role.ADMIN.name())
+                    .requestMatchers(HttpMethod.PUT, "/api/v1/products/**")
+                    .hasAuthority(Role.ADMIN.name())
+                    .requestMatchers(HttpMethod.DELETE, "/api/v1/products/**")
+                    .hasAuthority(Role.ADMIN.name())
+                    .requestMatchers(HttpMethod.POST, "/api/v1/categories")
+                    .hasAuthority(Role.ADMIN.name())
+                    .requestMatchers(HttpMethod.PUT, "/api/v1/categories")
+                    .hasAuthority(Role.ADMIN.name())
+                    .requestMatchers(HttpMethod.DELETE, "/api/v1/categories/**")
+                    .hasAuthority(Role.ADMIN.name())
+                    .requestMatchers(HttpMethod.POST, "/api/v1/images/upload")
+                    .hasAuthority(Role.ADMIN.name())
+                    .requestMatchers(HttpMethod.DELETE, "/api/v1/images")
+                    .hasAuthority(Role.ADMIN.name())
+                    .requestMatchers(
+                        HttpMethod.PATCH,
+                        "/api/v1/orders/{orderId}/delivery-status"
+                    )
+                    .hasAuthority(Role.ADMIN.name())
+                    .requestMatchers(
+                        HttpMethod.PATCH,
+                        "/api/v1/orders/payment/{paymentId}"
+                    )
+                    .hasAuthority(Role.ADMIN.name())
+                    .anyRequest()
+                    .authenticated()
+            )
+            .sessionManagement(session ->
+                session.sessionCreationPolicy(STATELESS)
+            )
+            .authenticationProvider(authenticationProvider)
+            .addFilterBefore(
+                jwtAuthFilter,
+                UsernamePasswordAuthenticationFilter.class
+            );
 
         return http.build();
     }
@@ -74,9 +141,9 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        UrlBasedCorsConfigurationSource source =
+            new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 }
-
